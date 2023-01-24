7d3f5aa2f035a322d18dccfb1ac4217a3ae5efe4
Milorad Stojanov <miloradstojanov93@gmail.com>
2023-01-24 10:11:59 +0100

    initial deploy

---
+++ dbinit.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MongoDB connected: ${conn.connection.host}`.america);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080 || process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`running on https://localhost:${PORT}`);
-});
+const connectDB = require("./dbinit");
+const cors = require("cors");
+require("colors");
+require("dotenv").config();
+connectDB();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("WELCOME TO MY STUPID API");
+});
+app.listen(PORT, () => {
+  console.log(`running on https://localhost:${PORT}`.america);
 });
